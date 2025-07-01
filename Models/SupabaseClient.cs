9188bff9bffdd8d2f1fa7d6e04da237586830833
kitten114 <qoo930114q@gmail.com>
2025-07-01 15:06:02 +0800

    Add files via upload

---
+++ Models/SupabaseClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace healthy.Models
+{
+    internal class SupabaseClient
+    {
+        // 連線登入 Supabase 共用函式
+        public Supabase.Client GetClient()
+        {
+            // 初始化 Supabase 客戶端
+            Supabase.Client _client;
+            _client = new Supabase.Client(
+                "https://你的Project ID.supabase.co",
+                "你的API Key",
+                new Supabase.SupabaseOptions { AutoConnectRealtime = true });
+            return _client;
+        }
+    }
+}
